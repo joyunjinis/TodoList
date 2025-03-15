869c12c27381a009b456ab479869d9526d4de025
joyunjinis <yjo671787@gmail.com>
2025-03-16 02:31:09 +0900

    second project

---
+++ src/components/List.jsx
@@ -0,0 +1,47 @@
+import "./List.css";
+import Todoitem from "./Todoitem";
+import { useState } from "react";
+
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLocaleLowerCase())
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
+  return (
+    <div className="List">
+      <h4>Todo List🍀</h4>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요."
+      />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return (
+            <Todoitem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
---
+++ src/components/Todoitem.jsx
@@ -0,0 +1,27 @@
+import "./Todoitem.css";
+
+const Todoitem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
+
+  const onClickDeleteButton = () => {
+    onDelete(id);
+  };
+
+  return (
+    <div className="Todoitem">
+      <input
+        onChange={onChangeCheckbox}
+        readOnly
+        checkbox={isDone}
+        type="checkbox"
+      />
+      <div className="content">{content}</div>
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
+      <button onClick={onClickDeleteButton}>삭제</button>
+    </div>
+  );
+};
+
+export default Todoitem;
